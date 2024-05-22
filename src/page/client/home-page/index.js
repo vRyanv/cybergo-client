ffb9084549df081ead458912182c69a41b8a8ae0
Ryan <89388361+vRyanv@users.noreply.github.com>
2024-05-22 23:21:56 +0700

    config balel build

--- src/page/client/home-page/index.js
+++ src/page/client/home-page/index.js
@@ -1,7 +1,13 @@
 import {UseDocumentTitle} from "~/hooks";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export default function HomePage(){
     UseDocumentTitle('Home')
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate('/sign-in')
+    }, []);
     return (
         <div>
             {/*home page*/}
