b11cc7532503fa51e34b4280dc643ebb6dc001bd
ryan <89388361+vRyanv@users.noreply.github.com>
2024-11-12 22:27:23 +0700

    update auth

--- src/service/socket/SocketService.js
+++ src/service/socket/SocketService.js
@@ -1,45 +1,40 @@
-import {createContext, useContext} from 'react'
-import {io} from "socket.io-client";
-import {FieldName, Http} from "~/constants";
-import {UseLocalStorage} from "~/hooks";
-import {SocketEvent} from "~/service/socket/SocketEvent";
+import { createContext, useContext } from "react";
+import { io } from "socket.io-client";
+import { FieldName, Http } from "~/constants";
+import { UseLocalStorage } from "~/hooks";
+import { SocketEvent } from "~/service/socket/SocketEvent";
 
 const SocketContext = createContext();
 export const useSocket = () => useContext(SocketContext);
-const socket = io(
-    Http.HOST,
-    {
-        autoConnect: false,
-    }
-)
-export default function SocketService({children }){
-    const [getLocal] = UseLocalStorage()
-    const token = getLocal(FieldName.USER_TOKEN)
-    if(!socket.connected && token){
-        socket.auth = {token}
-        socket.autoConnect = true
-        socket.connect()
-    }
-    socket.on('connect', () => {
-        console.log('socket connect: ',socket.id)
-    })
-    socket.on('connect_error', (error) => {
-        console.log('socket connect_error: ',error.message)
-        socket.off(SocketEvent.NOTIFICATION);
-    })
+const socket = io(Http.HOST, {
+  autoConnect: false,
+});
+export default function SocketService({ children }) {
+  const [getLocal] = UseLocalStorage();
+  const token = getLocal(FieldName.USER_TOKEN);
+  if (!socket.connected && token) {
+    socket.authorization = token;
+    socket.autoConnect = true;
+    socket.connect();
+  }
+  socket.on("connect", () => {
+    console.log("socket connect: ", socket.id);
+  });
+  socket.on("connect_error", (error) => {
+    console.log("socket connect_error: ", error.message);
+    socket.off(SocketEvent.NOTIFICATION);
+  });
 
-    socket.on('reconnect', (number)=> {
-        console.log("socket reconnect: ", number)
-    })
+  socket.on("reconnect", (number) => {
+    console.log("socket reconnect: ", number);
+  });
 
-    socket.on('disconnect', (reason)=>{
-        console.log("socket disconnect reason: ", reason)
-        socket.off(SocketEvent.NOTIFICATION);
-    })
+  socket.on("disconnect", (reason) => {
+    console.log("socket disconnect reason: ", reason);
+    socket.off(SocketEvent.NOTIFICATION);
+  });
 
-    return (
-        <SocketContext.Provider value={socket}>
-            {children }
-        </SocketContext.Provider>
-    )
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
 }
